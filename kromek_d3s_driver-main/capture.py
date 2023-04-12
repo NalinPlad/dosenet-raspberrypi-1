138c32c6c2a540931cfcc3a381e539e5fb690755
alihanks <j.ali.hanks@gmail.com>
2023-04-12 12:47:44 -0700

    new code for rad sensor daq

---
+++ kromek_d3s_driver-main/capture.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import kromek
+import TimerLoop
+import datetime
+
+base_config = {
+    "upload_period": 10,
+    # "config_check_period": 7200,
+    # "ping_period": 900,
+    "tick_length": 0.5,
+    # "sensor_params": {},
+    # "max_consec_net_errs": 10,
+    # "mail_check_period": 7200,
+    # "bground_check_period": 5,
+}
+
+
+def pre_run():
+    kdevs = kromek.discover()
+    print(f"Discovered {kdevs}")
+    if len(kdevs) <= 0:
+        return
+
+    try:
+        kconn = kromek.connect(kdevs[0])
+    except Exception:
+        return
+
+    cfg = {k: base_config[k] for k in base_config}
+    cfg["kconn"] = kconn
+
+    return cfg
+
+
+def read_sensor(cfg):
+    print("read_sensor()")
+    fake_kromek = False
+
+    sdata = {}
+    if fake_kromek:
+        sdata = {
+            "serial": "blee blop",
+            "bias": 123,
+            "measurement": [1, 2, 3, 4, 5, 6, 7],
+        }
+    else:
+        for group in [
+            "serial",
+            "status",
+            "measurement",
+            "gain",
+            "bias",
+            "lld-g",
+            "lld-n",
+        ]:
+            res = kromek.get_value(cfg["kconn"], param=group)
+
+            """
+            Gets the timestamp for the measurement. 
+            This parameter could be added to the Kromek package.
+            """
+            if group == "measurement":
+                res["timestamp"] = datetime.datetime.now()
+
+            for k in res:
+                sdata[k] = res[k]
+    return sdata
+
+
+class CapHandlers(object):
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def take_reading(self, name, now):
+        sdata = read_sensor(self.cfg)
+        print(sdata)
+        return False
+
+
+def main(cfg):
+    ch = CapHandlers(cfg)
+    te = TimerLoop.TimerLoop()
+
+    te.addHandler(ch.take_reading, cfg["upload_period"])
+    te.run(cfg["tick_length"])
+
+
+if __name__ == "__main__":
+    try:
+        cfg = pre_run()
+        if cfg:
+            main(cfg)
+    except Exception as e:
+        print("Whoops!", e)
