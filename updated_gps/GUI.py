e22f440ce792de2e55dd40c60701453fccebcded
alihanks <j.ali.hanks@gmail.com>
2023-04-13 14:16:06 -0700

    testing

--- updated_gps/GUI.py
+++ updated_gps/GUI.py
@@ -298,11 +298,11 @@ class plottingWidget(QWidget):
 			self.sendMessage('Time Delay', self.timeDelay, 'control')
 			self.sendMessage('Files', self.files, 'control')
 			self.sendMessage('Shown Sensor', self.sensorRadioButtons.selectedButton, 'control')
-			child = pexpect.spawn('sudo python /home/pi/dosenet-raspberrypi-1/updated_gps/map_plot.py')
-			child.expect('password for pi:')
-			child.sendline('piistasty!')
-			child.interact()
-			#os.system('python /home/pi/dosenet-raspberrypi-1/updated_gps/map_plot.py &')
+			#child = pexpect.spawn('sudo python /home/pi/dosenet-raspberrypi-1/updated_gps/map_plot.py')
+			#child.expect('password for pi:')
+			#child.sendline('piistasty!')
+			#child.interact()
+			os.system('python /home/pi/dosenet-raspberrypi-1/updated_gps/map_plot.py &')
 			self.sensorRadioButtons.started = True
 		elif self.sensorRadioButtons.started:
 			print("Already sent message to start.")
--- updated_gps/map_plot.py
+++ updated_gps/map_plot.py
@@ -165,11 +165,11 @@ def establish_dict():
 			sendmsg('P/T/H', 'START', 'fromGUI')
 	
 		if not radiationRunning and sensor in ['Radiation (cps)', 'Radiation Bi (cps)', 'Radiation K (cps)', 'Radiation Tl (cps)']:
-			#child = pexpect.spawn('sudo python /home/pi/dosenet-raspberrypi-1/D3S_rabbitmq_DAQ.py -i '+str(time_delay))
-			os.system('sudo python /home/pi/dosenet-raspberrypi-1/D3S_rabbitmq_DAQ.py -i ' + str(time_delay) + ' &')
-			#child.expect('password for pi:')
-			#child.sendline('piistasty!')
-			#child.interact()
+			child = pexpect.spawn('sudo python /home/pi/dosenet-raspberrypi-1/D3S_rabbitmq_DAQ.py -i '+str(time_delay))
+			#os.system('sudo python /home/pi/dosenet-raspberrypi-1/D3S_rabbitmq_DAQ.py -i ' + str(time_delay) + ' &')
+			child.expect('password for pi:')
+			child.sendline('piistasty!')
+			child.interact()
 			sendmsg('Radiation', 'START', 'fromGUI')
 			radiationRunning = True
 	
