515267268afb85ec3e6106efd5350721c590f85e
alihanks <j.ali.hanks@gmail.com>
2023-05-22 14:46:44 -0700

    updating sensor/display code

--- weather_rabbitmq_DAQ.py
+++ weather_rabbitmq_DAQ.py
@@ -16,14 +16,16 @@ import json
 sys.stdout.flush()
 
 class weather_DAQ(object):
-    def __init__(self, interval=1, datalog=None):
+    def __init__(self, interval=1, datalog=None, test=False):
         self.n_merge=int(interval)
-
+        self.test_mode = test
         self.outfile_name = datalog
 
         self.temp_list=[]
         self.humid_list=[]
         self.press_list=[]
+        self.alt_list=[]
+        self.voc_list=[]
         i2c = board.I2C()
         self.sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
@@ -41,11 +43,13 @@ class weather_DAQ(object):
             atm = pascals/101325.0 * 100
             humidity = self.sensor.relative_humidity
             altitude = self.sensor.altitude
+            voc = self.sensor.gas
 
             self.temp_list.append(degrees)
             self.humid_list.append(humidity)
-            self.press_list.append(altitude)
-
+            self.press_list.append(pascals)
+            self.alt_list.append(altitude)
+            self.voc_list.append(voc)
 
             if len(self.temp_list)>=self.n_merge:
                 t_data = [np.mean(np.asarray(self.temp_list)),
@@ -54,10 +58,17 @@ class weather_DAQ(object):
                            np.std(np.asarray(self.humid_list))]
                 p_data = [np.mean(np.asarray(self.press_list)),
                            np.std(np.asarray(self.press_list))]
-                self.send_data('P/T/H',[t_data,h_data,p_data])
-                print("Data being sent to GUI: {}".format([t_data,h_data,p_data]))
-                if self.out_file is not None:
-                    self.write_data(t_data, h_data, p_data)
+                a_data = [np.mean(np.asarray(self.alt_list)),
+                           np.std(np.asarray(self.alt_list))]
+                v_data = [np.mean(np.asarray(self.voc_list)),
+                           np.std(np.asarray(self.voc_list))]
+                if not self.test_mode:
+                    self.send_data('P/T/H',[t_data,h_data,p_data,a_data,v_data])
+                    print("Data being sent to GUI: {}".format([t_data,h_data,p_data,a_data,v_data]))
+                    if self.out_file is not None:
+                        self.write_data(t_data, h_data, p_data, a_data, v_data)
+                else:
+                    print("Data collected: T={}, H={}, P={}, alt={}, gas={}".format(t_data[0],h_data[0],p_data[0],a_data[0],v_data[0]))
                 sys.stdout.flush()
                 self.clear_data()
 
@@ -82,6 +93,8 @@ class weather_DAQ(object):
         self.temp_list[:] = []
         self.humid_list[:] = []
         self.press_list[:] = []
+        self.alt_list[:] = []
+        self.voc_list[:] = []
 
 
     def receive(self):
@@ -105,12 +118,17 @@ class weather_DAQ(object):
     def create_file(self, fname = None):
         self.out_file = open(fname, "a",newline="")
         self.results = csv.writer(self.out_file, delimiter = ",")
-        metadata = ["Time", "Temperature", "T Unc.", "Humidity", "H Unc.", "Pressure", "P Unc."]
+        metadata = ["Time", 
+                    "Temperature", "T Unc.", 
+                    "Humidity", "H Unc.", 
+                    "Pressure", "P Unc.", 
+                    "Altitude", "A Unc.", 
+                    "Gas", "G Unc."]
         self.results.writerow(metadata)
 
-    def write_data(self, data1, data2, data3):
+    def write_data(self, data1, data2, data3, data4, data5):
         this_time = time.time()
-        self.results.writerow([this_time] + data1[:] + data2[:] + data3[:])
+        self.results.writerow([this_time] + data1[:] + data2[:] + data3[:] + data4[:] + data5[:])
 
     def close_file(self):
         self.out_file.close()
@@ -127,6 +145,7 @@ if __name__ == '__main__':
     parser.add_argument(
         "--interval", "-i", type=int, default=1)
     parser.add_argument('--datalog', '-d', default=None)
+    parser.add_argument('--test', '-t', action='store_true', default=False)
 
     args = parser.parse_args()
     arg_dict = vars(args)
@@ -134,6 +153,11 @@ if __name__ == '__main__':
     daq = weather_DAQ(**arg_dict)
     while True:
         # Look for messages from GUI every 10 ms
+        if args.test:
+            daq.run()
+            time.sleep(1)
+            continue
+
         msg = daq.receive()
         print("P/T/H received msg: {}".format(msg))
         sys.stdout.flush()
