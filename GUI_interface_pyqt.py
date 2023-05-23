bef64624e8803397a058c3491408f68da898c5ba
alihanks <j.ali.hanks@gmail.com>
2023-05-23 14:37:55 -0700

    bug fix

--- GUI_interface_pyqt.py
+++ GUI_interface_pyqt.py
@@ -934,11 +934,13 @@ class App(QWidget):
 
     def close_file(self):
         self.out_file.close()
+        '''
         print("Copying data from {} to server.".format(self.out_file.name))
         sys_cmd = 'scp {} pi@192.168.4.1:/home/pi/data/'.format(
                                 self.out_file.name)
         err = os.system(sys_cmd)
         print("system command returned {}".format(err))
+        '''
         sys.stdout.flush()
 
     @pyqtSlot()
