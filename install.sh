338980be9bbd34dc598c7d4a18217dc393eee81d
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-07-20 15:52:15 -0700

    add installer script

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Installing everything... make sure to run with sudo"
+
+sudo apt-get -y update
+sudo apt-get -u upgrade
+sudo apt-get -y install python3-pip
+
+pip3 install -r requirements.txt
