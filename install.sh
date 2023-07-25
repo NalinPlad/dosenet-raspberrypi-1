da4af60fc4e36f65ae3fe17e7756b0a0e543d861
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-07-25 10:56:24 -0700

    cp files

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 echo "Installing everything... make sure to run with sudo"
 
-git pull
+sudo -Es
 
+git pull --force
+
+# Update and upgrade system
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install python3-pip
 
+# Install dependancies
 pip3 install -r requirements.txt
+
+# Copy all .desktop files to desktop
+cp *.desktop /home/pi/Desktop
